c33c76f761b6a0d4936d6cdeb9b8ccfd488db4ff
Erkan <ErkanOzulu@users.noreply.github.com>
2024-04-20 16:33:59 +0300

    Initial commit

---
+++ src/test/java/com/mycomp/tests/App.java
@@ -0,0 +1,36 @@
+package com.mycomp.tests;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+        }
+
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.webkit().launch();
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev/");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        }
+
+
+        /**
+         * By default, Playwright runs the browsers in headless mode. To see the browser UI, pass the setHeadless(false) flag while launching the browser. You can also use slowMo to slow down execution. Learn more in the debugging tools section.
+
+         * playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+         */
+
+    }
+}
+
---
+++ src/test/java/com/mycomp/tests/P01_PlayWrigtTest.java
@@ -0,0 +1,24 @@
+package com.mycomp.tests;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class P01_PlayWrigtTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        Playwright playwright = Playwright.create();
+        BrowserType chromium = playwright.chromium();
+        Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("http://google.com");
+
+        Thread.sleep(3000);
+        page.close();
+        browser.close();
+        playwright.close();
+
+    }
+
+}
