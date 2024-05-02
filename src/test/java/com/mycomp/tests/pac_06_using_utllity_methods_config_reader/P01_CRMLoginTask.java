dce940b3e125ff78ba4d7cddc1bf81ca59170f41
Erkan <ErkanOzulu@users.noreply.github.com>
2024-05-02 08:20:14 +0300

    CRMUtils

---
+++ src/test/java/com/mycomp/tests/pac_06_using_utllity_methods_config_reader/P01_CRMLoginTask.java
@@ -0,0 +1,61 @@
+package com.mycomp.tests.pac_06_using_utllity_methods_config_reader;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.mycomp.utils.CRMUtils;
+import org.junit.jupiter.api.*;
+
+public class P01_CRMLoginTask {
+
+
+    static private Playwright playwright;
+
+    static private Browser browser;
+
+    private Page page;
+
+    @BeforeAll
+    static void beforeAll() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                .setHeadless(false)
+
+        );
+    }
+
+
+    @AfterAll
+    static void afterAll() {
+
+        browser.close();
+        playwright.close();
+    }
+
+
+    @BeforeEach
+    void setUp() {
+        page = browser.newContext().newPage();
+        page.navigate("https://login2.nextbasecrm.com/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        page.close();
+    }
+
+
+    @Test
+    void test1() {
+
+        CRMUtils.login(page);
+
+
+    }
+
+    @Test
+    void test2() {
+        CRMUtils.login(page,"marketing1@cydeo.com","UserUser");
+    }
+}
